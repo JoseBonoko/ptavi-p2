9efeda2396ae0ec1fdcce8d637b151f3a764ea99
Jose Manuel Bonoko Bijeri <jbonoko@gamma15.aulas.gsyc.urjc.es>
2018-10-01 20:56:09 +0200

    programa calcplusplus.py

---
+++ calcplusplus.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcplus
+import csv
+
+if __name__ == "__main__":
+    with open(sys.argv[1], newline='') as f:
+        fich = open(sys.argv[1], 'r', encoding='utf-8')
+        reader = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
+        lineas = fich.readlines()
+        calcul = calcplus.calcoohija.CalculadoraHija()
+        for linea in lineas:
+            elementos = linea.split(',')
+            if elementos[0] == "suma":
+                resultsuma = 0
+                for elemento in elementos[1:]:
+                    elemento = int(elemento)
+                    resultsuma = calcul.plus(resultsuma, elemento)
+                print(linea)
+                print(resultsuma)
+            elif elementos[0] == "resta":
+                resultresta = elementos[1]
+                for elemento in elementos[2:]:
+                    resultresta = int(resultresta)
+                    elemento = int(elemento)
+                    resultresta = calcul.minus(resultresta, elemento)
+                print(linea)
+                print(resultresta)
+            elif elementos[0] == "multiplica":
+                resultmulti = elementos[1]
+                for elemento in elementos[2:]:
+                    resultmulti = int(resultmulti)
+                    elemento = int(elemento)
+                    resultmulti = calcul.multi(resultmulti, elemento)
+                print(linea)
+                print(resultmulti)
+            elif elementos[0] == "divide":
+                resultdivision = elementos[1]
+                for elemento in elementos[2:]:
+                    resultdivision = int(resultdivision)
+                    elemento = int(elemento)
+                    if resultdivision == 0 or elemento == 0:
+                        sys.exit("Division by zero is not allowed")
+                    else:
+                        resultdivision = calcul.divide(resultdivision, elemento)
+                print(linea)
+                print(resultdivision)
+            else:
+                sys.exit('Sólo se puede sumar, restar, multiplicar o dividir')
